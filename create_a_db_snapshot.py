5d588376012d6e4e594e437ef258005f46e4d1cf
ian douglas <ian.douglas@iandouglas.com>
2024-07-16 14:45:03 -0600

    updated database snapshot script to try to avoid race conditions on timing

--- create_a_db_snapshot.py
+++ create_a_db_snapshot.py
@@ -1,11 +1,6 @@
 '''
-Render provides a [database snapshot once per day](https://docs.render.com/postgresql-backups) for your managed databases, but you can use the API to create your own snapshots whenever you like. This can be useful for creating backups before making changes to your database schema, or for creating a snapshot before running a large migration.
-
-This example shows you how to create a snapshot of a Render managed database using the Render API.
-
-The `pg_dump` utility cannot take a password parameter. You will need to set up a `.pgpass` file to handle the authentication. You can read more about this in the PostgreSQL documentation at https://www.postgresql.org/docs/current/libpq-pgpass.html
-(This .pgpass file is only used to match the password with other details like hostname, username, and database name used below)
-
+Demonstrates using the Render API to start a create a database
+snapshot and download it.
 '''
 from auth import post_request, get_request
 import time
@@ -13,7 +8,6 @@ import requests
 import urllib.parse
 
 database_id = "db-1a2b3c4d5e6f"
-database_id = 'dpg-cqaq8mt6l47c73cl96s0-a'
 backup_endpoint = f"/postgres/{database_id}/backup"
 
 # get the current date/time without seconds in UTC
@@ -35,7 +29,7 @@ while not done:
     print(payload)
     if success:
         for backup in payload:
-            if backup["createdAt"] == backup_timestamp:
+            if backup["createdAt"] >= backup_timestamp:
                 backup_url = backup["url"]
                 done = True
                 break
